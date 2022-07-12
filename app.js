d6e9f73865040486ba0148ade3eefc5b594ef431
KunalP99 <kunal_patel200@hotmail.co.uk>
2022-07-12 15:51:09 +0100

    Created a function which plays a round of the game

--- app.js
+++ app.js
@@ -5,15 +5,38 @@ const computerPlay = () => {
     // Assign number to the three results
     switch(rand) {
         case 1: 
-            result = "Rock";
+            result = "ROCK";
             break;
         case 2: 
-            result = "Paper";
+            result = "PAPER";
             break;
         case 3: 
-            result = "Scissors";
+            result = "SCISSORS";
             break;
     }
     return result;
-}
-console.log(computerPlay());
+};
+
+const playRound = (playerSelection, computerSelection) => {
+    // Make every player selection uppercase to avoid errors
+    let caseInsensitive = playerSelection.toUpperCase();
+    if (caseInsensitive == computerSelection) {
+        return `It's a tie! You both chose ${computerSelection}.`;
+    } else if (caseInsensitive === "ROCK" && computerSelection === "PAPER") {
+        return `You Lose! PAPER beats ROCK.`;
+    } else if (caseInsensitive === "ROCK" && computerSelection === "SCISSORS") {
+        return `You Win! ROCK beats SCISSORS.`;
+    } else if (caseInsensitive === "PAPER" && computerSelection === "SCISSORS") {
+        return `You Lose! SCISSORS beats PAPER.`;
+    } else if (caseInsensitive === "SCISSORS" && computerSelection === "PAPER") {
+        return `You Win! SCISSORS beats PAPER.`;
+    }
+}; 
+
+const game = () => {
+    
+};
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
